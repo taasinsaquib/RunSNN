7aa5a2e221c16977b08a5a5562660b47510b46b7
Taasin Saqib <taasin.saquib@gmail.com>
2021-12-02 13:02:55 -0800

    create delta ONV

--- main.py
+++ main.py
@@ -18,23 +18,45 @@ def getOnv(path):
 def main():
 
 	# TODO: pass path as cmd line arg
-	path    = 'C:/Users/taasi/Desktop/process'
+	path    = 'C:/Users/taasi/Desktop/RunSNN/files'
 	onvPath = f'{path}/onvOut.csv'
 	resPath = f'{path}/resultOut.csv'
 
-	# read from ONV file
-	onv = getOnv(onvPath)
-	print(onv.shape)
 
-	# clear ONV file
-	open(onvPath, 'w').close()
+	# ****** CREATE DELTAONV ****** #
+
+	# read from ONV file, should be shape (2, #photoreceptors)
+	# first row is prev onv, second row is cur one
+	
+	prevOnv = None
+	curOnv  = None
+
+	curOnvString = None		# save string version to easily write back later
+
+	with open(onvPath, 'r') as f:
+		for i, line in enumerate(f):
+			if (i == 1):
+				curOnvString = line
+				curOnv  = np.fromstring(line, sep=',')
+			else:
+				prevOnv = np.fromstring(line, sep=',') 
+
+	deltaOnv = curOnv - prevOnv
+
+	# only keep curOnv in file
+	with open(onvPath, 'w') as f:
+		f.write(curOnvString)
+
+
+	# ********* RUN MODEL ********* #
+	# TODO: use the real models
 
 	# setup and run the model
 	model = FC()
 	model.float()
 	model.to(device)
 
-	inputs = torch.from_numpy(onv)
+	inputs = torch.from_numpy(deltaOnv)
 	inputs = inputs.float()
 
 	output = model(inputs)
@@ -44,7 +66,8 @@ def main():
 
 	# write results to file
 	with open(resPath, 'w') as the_file:
-	    the_file.write(f'{ output[0] }, { output[1] }')
+		the_file.write(f'{ output[0] }, { output[1] }')
+
 
 if __name__ == "__main__":
 	main()
